76cd3169d0398f412512ac0aa07eef7de3d9e3e8
Artem Shoobovych <shybovycha@gmail.com>
2013-10-06 13:59:16 +0300

    Started an empty Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TicketTracking::Application.config.secret_key_base = '5e6b7838097e8c730a5412b3be3b838476ac7d6ca2d2a019cd9c52492ca0ea1add5e446bc1dfdccd2f7c3862624d3eba2d37e4cdfbe807502f79ee76dfd30a21'
